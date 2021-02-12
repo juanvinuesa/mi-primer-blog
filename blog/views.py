fb75e8e6d31421c7aeaec90bfdcec9ab56e0fe91
juanvinuesa <juanvinuesa12@hotmail.com>
2021-02-12 12:00:33 -0300

    agregamos caja de comentarios sin moderacion

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from .forms import PostForm
+from .forms import CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -29,3 +30,15 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
